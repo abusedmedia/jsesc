3182ce78bbfb1edfaf91d1edac03592f9af9c90e
abusedmedia <ff@todo.to.it>
2018-07-31 12:40:20 +0200

    minor

--- jsesc.js
+++ jsesc.js
@@ -32,16 +32,16 @@ const toString = object.toString
 const isArray = Array.isArray
 // const isBuffer = Buffer.isBuffer;
 const isObject = (value) => {
-	// This is a very simple check, but it’s good enough for what we need.
+  // This is a very simple check, but it’s good enough for what we need.
   return toString.call(value) == '[object Object]'
 }
 const isString = (value) => {
   return typeof value === 'string' ||
-		toString.call(value) == '[object String]'
+    toString.call(value) == '[object String]'
 }
 const isNumber = (value) => {
   return typeof value === 'number' ||
-		toString.call(value) == '[object Number]'
+    toString.call(value) == '[object Number]'
 }
 const isFunction = (value) => {
   return typeof value === 'function'
@@ -65,8 +65,8 @@ const singleEscapes = {
   '\n': '\\n',
   '\r': '\\r',
   '\t': '\\t'
-	// `\v` is omitted intentionally, because in IE < 9, '\v' == 'v'.
-	// '\v': '\\x0B'
+  // `\v` is omitted intentionally, because in IE < 9, '\v' == 'v'.
+  // '\v': '\\x0B'
 }
 const regexSingleEscape = /["'\\\b\f\n\r\t]/
 
@@ -79,7 +79,7 @@ const jsesc = (argument, options) => {
     ++options.indentLevel
     indent = options.indent.repeat(options.indentLevel)
   }
-	// Handle options
+  // Handle options
   const defaults = {
     'escapeEverything': false,
     'minimal': false,
@@ -103,18 +103,18 @@ const jsesc = (argument, options) => {
   }
   options = extend(defaults, options)
   if (
-		options.quotes != 'single' &&
-		options.quotes != 'double' &&
-		options.quotes != 'backtick'
-	) {
+    options.quotes != 'single' &&
+    options.quotes != 'double' &&
+    options.quotes != 'backtick'
+  ) {
     options.quotes = 'single'
   }
   const quote = options.quotes == 'double'
-		? '"'
-		: (options.quotes == 'backtick'
-			? '`'
-			: '\''
-		)
+    ? '"'
+    : (options.quotes == 'backtick'
+      ? '`'
+      : '\''
+    )
   const compact = options.compact
   const lowercaseHex = options.lowercaseHex
   let indent = options.indent.repeat(options.indentLevel)
@@ -150,12 +150,12 @@ const jsesc = (argument, options) => {
       }
       return 'new Set(' + jsesc(Array.from(argument), options) + ')'
     }
-		// if (isBuffer(argument)) {
-		// 	if (argument.length == 0) {
-		// 		return 'Buffer.from([])';
-		// 	}
-		// 	return 'Buffer.from(' + jsesc(Array.from(argument), options) + ')';
-		// }
+    // if (isBuffer(argument)) {
+    //  if (argument.length == 0) {
+    //    return 'Buffer.from([])';
+    //  }
+    //  return 'Buffer.from(' + jsesc(Array.from(argument), options) + ')';
+    // }
     if (isArray(argument)) {
       result = []
       options.wrap = true
@@ -172,9 +172,9 @@ const jsesc = (argument, options) => {
           options.__inline2__ = false
         }
         result.push(
-					(compact || inline2 ? '' : indent) +
-					jsesc(value, options)
-				)
+          (compact || inline2 ? '' : indent) +
+          jsesc(value, options)
+        )
       })
       if (isEmpty) {
         return '[]'
@@ -183,10 +183,10 @@ const jsesc = (argument, options) => {
         return '[' + result.join(', ') + ']'
       }
       return '[' + newLine + result.join(',' + newLine) + newLine +
-				(compact ? '' : oldIndent) + ']'
+        (compact ? '' : oldIndent) + ']'
     } else if (isNumber(argument)) {
       if (json) {
-				// Some number values (e.g. `Infinity`) cannot be represented in JSON.
+        // Some number values (e.g. `Infinity`) cannot be represented in JSON.
         return JSON.stringify(argument)
       }
       if (useDecNumbers) {
@@ -207,9 +207,9 @@ const jsesc = (argument, options) => {
       }
     } else if (!isObject(argument)) {
       if (json) {
-				// For some values (e.g. `undefined`, `function` objects),
-				// `JSON.stringify(value)` returns `undefined` (which isn’t valid
-				// JSON) instead of `'null'`.
+        // For some values (e.g. `undefined`, `function` objects),
+        // `JSON.stringify(value)` returns `undefined` (which isn’t valid
+        // JSON) instead of `'null'`.
         return JSON.stringify(argument) || 'null'
       }
       return String(argument)
@@ -220,22 +220,22 @@ const jsesc = (argument, options) => {
       forOwn(argument, (key, value) => {
         isEmpty = false
         result.push(
-					(compact ? '' : indent) +
-					jsesc(key, options) + ':' +
-					(compact ? '' : ' ') +
-					jsesc(value, options)
-				)
+          (compact ? '' : indent) +
+          jsesc(key, options) + ':' +
+          (compact ? '' : ' ') +
+          jsesc(value, options)
+        )
       })
       if (isEmpty) {
         return '{}'
       }
       return '{' + newLine + result.join(',' + newLine) + newLine +
-				(compact ? '' : oldIndent) + '}'
+        (compact ? '' : oldIndent) + '}'
     }
   }
 
   const string = argument
-	// Loop over each code unit in the string and escape it
+  // Loop over each code unit in the string and escape it
   let index = -1
   const length = string.length
   result = ''
@@ -244,12 +244,12 @@ const jsesc = (argument, options) => {
     if (options.es6) {
       const first = string.charCodeAt(index)
       if ( // check if it’s the start of a surrogate pair
-				first >= 0xD800 && first <= 0xDBFF && // high surrogate
-				length > index + 1 // there is a next code unit
-			) {
+        first >= 0xD800 && first <= 0xDBFF && // high surrogate
+        length > index + 1 // there is a next code unit
+      ) {
         const second = string.charCodeAt(index + 1)
         if (second >= 0xDC00 && second <= 0xDFFF) { // low surrogate
-					// https://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
+          // https://mathiasbynens.be/notes/javascript-encoding#surrogate-formulae
           const codePoint = (first - 0xD800) * 0x400 + second - 0xDC00 + 0x10000
           let hexadecimal = codePoint.toString(16)
           if (!lowercaseHex) {
@@ -263,8 +263,8 @@ const jsesc = (argument, options) => {
     }
     if (!options.escapeEverything) {
       if (regexWhitelist.test(character)) {
-				// It’s a printable ASCII character that is not `"`, `'` or `\`,
-				// so don’t escape it.
+        // It’s a printable ASCII character that is not `"`, `'` or `\`,
+        // so don’t escape it.
         result += character
         continue
       }
@@ -282,15 +282,15 @@ const jsesc = (argument, options) => {
       }
     }
     if (
-			character == '\0' &&
-			!json &&
-			!regexDigit.test(string.charAt(index + 1))
-		) {
+      character == '\0' &&
+      !json &&
+      !regexDigit.test(string.charAt(index + 1))
+    ) {
       result += '\\0'
       continue
     }
     if (regexSingleEscape.test(character)) {
-			// no need for a `hasOwnProperty` check here
+      // no need for a `hasOwnProperty` check here
       result += singleEscapes[character]
       continue
     }
@@ -305,7 +305,7 @@ const jsesc = (argument, options) => {
     }
     const longhand = hexadecimal.length > 2 || json
     const escaped = '\\' + (longhand ? 'u' : 'x') +
-			('0000' + hexadecimal).slice(longhand ? -4 : -2)
+      ('0000' + hexadecimal).slice(longhand ? -4 : -2)
     result += escaped
     continue
   }
@@ -316,14 +316,14 @@ const jsesc = (argument, options) => {
     result = result.replace(/\$\{/g, '\\\$\{')
   }
   if (options.isScriptContext) {
-		// https://mathiasbynens.be/notes/etago
+    // https://mathiasbynens.be/notes/etago
     return result
-			.replace(/<\/(script|style)/gi, '<\\/$1')
-			.replace(/<!--/g, json ? '\\u003C!--' : '\\x3C!--')
+      .replace(/<\/(script|style)/gi, '<\\/$1')
+      .replace(/<!--/g, json ? '\\u003C!--' : '\\x3C!--')
   }
   return result
 }
 
 jsesc.version = '2.5.1'
 
-module.exports = jsesc
+if (window && window.module) module.exports = jsesc
